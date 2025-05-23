9bbe555ed34a45a79b0a5c25685f58aface340d3
Piyush <piyxsh@gmail.com>
2025-05-24 02:35:09 +1000

    changed PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors({
     origin: 'https://www.piyushj.dev'
