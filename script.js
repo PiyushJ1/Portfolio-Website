172737c6041f134b3d7a5a1cfc6c54de9830af25
Piyush <piyxsh@gmail.com>
2025-05-24 02:14:37 +1000

    fd

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById('send-button').addEventListener('click', async () => {
     const data = {
-            firstName: document.querySelector('input[placeholder="First Name"]').value,
-            lastName: document.querySelector('input[placeholder="Last Name"]').value,
-            email: document.querySelector('input[placeholder="Email"]').value,
-            message: document.querySelector('input[placeholder="Your Message"]').value,
+        firstName: document.querySelector('input[placeholder="First Name"]').value,
+        lastName: document.querySelector('input[placeholder="Last Name"]').value,
+        email: document.querySelector('input[placeholder="Email"]').value,
+        message: document.querySelector('textarea[placeholder="Your Message"]').value,
     };
 
 
