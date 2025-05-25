999e32c637c959fab7f55556ac608400a381621f
Piyush <piyxsh@gmail.com>
2025-05-26 00:36:23 +1000

    created more sections in index.html

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
-require('dotenv').config();
-
 const app = express();
 const PORT = process.env.PORT || 4000;
 
@@ -11,6 +11,8 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.static('public'));
+
+
 
 app.post('/send-message', async (req, res) => {
     console.log('Incoming message:', req.body);
@@ -30,7 +32,7 @@ app.post('/send-message', async (req, res) => {
             from: email,
             to: process.env.EMAIL_USER,
             subject: `Message from ${firstName} ${lastName}`,
-            text: `Email: ${email}\n\nMessage:\n${message}`
+            text: `From: ${email}\n\nMessage:\n${message}`
         };
 
         await transporter.sendMail(mailOptions);
